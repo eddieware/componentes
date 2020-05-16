53a7f06d6b39e86389313e08a302b49e6efe54e5
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2020-05-16 11:39:12 -0500

    cambio de pagina

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:componentes/src/pages/home_temp.dart';
+import 'package:componentes/src/pages/home_page.dart';
+//import 'package:componentes/src/pages/home_temp.dart';
 import 'package:flutter/material.dart';
  
 void main() => runApp(MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes App',
       debugShowCheckedModeBanner: false,
-      home: HomePageTemp()
+      home: HomePage()
       
     );
   }
