cd2856da0cb33d33f5b989e785cfae2e3c817787
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2020-05-20 14:02:44 -0500

    avatar_page

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,7 +6,41 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+
+          Container(
+             padding:EdgeInsets.all(8.0),
+             child: CircleAvatar(
+              backgroundImage:NetworkImage('https://www.elsoldeacapulco.com.mx/incoming/wpgvi0-villanos-creados-por-stan-lee.jpg/ALTERNATES/LANDSCAPE_1140/Villanos%20creados%20por%20Stan%20lee.jpg'),
+              radius: 20.0,
+              
+            ),
+          ),
+
+
+          Container(
+
+            margin: EdgeInsets.only(right:10.0),// se uso como padding lateral
+            child: CircleAvatar(
+
+              child: Text('SL'),
+              backgroundColor: Colors.purple
+
+            ),
+          )
+        ],
       ),
+
+
+      body: Center(
+        child:FadeInImage(
+          image: NetworkImage('https://fotos02.laopiniondemalaga.es/2018/11/24/1024x341/stan-lee.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds:200),
+          
+        )
+      ),
+      
     );
   }
 }
