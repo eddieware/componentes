dcc9d8619dc4891324ab59cd8c31e9b34c4f0cd7
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2020-05-24 10:48:12 -0500

    formato_fecha_español

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/routes/routes.dart';
 import 'package:flutter/material.dart';
+
+import 'package:flutter_localizations/flutter_localizations.dart';
  
 void main() => runApp(MyApp());
  
@@ -10,6 +12,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes App',
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+      // ... app-specific localization delegate[s] here
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate,
+    ],
+    supportedLocales: [
+    const Locale('en'), // English
+    const Locale('es'), // Hebrew
+    const Locale.fromSubtags(languageCode: 'zh'), // Chinese *See Advanced Locales below*
+    // ... other locales the app supports
+    ],
      // home: HomePage()
      initialRoute: '/',
       routes:getApplicationRoutes(),
