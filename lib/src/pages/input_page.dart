54627e4ca47c925cd8223ff0e949b9d1e93d2f88
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2020-05-24 09:47:49 -0500

    password field

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
-
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 //https://www.netflix.com/watch/80185236?trackId=155573560
 class _InputPageState extends State<InputPage> {
+   String _email='';
   String _nombre='';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +22,10 @@ class _InputPageState extends State<InputPage> {
         children: <Widget>[
           _crearInput(),
           Divider(),
+          _crearEmail(),
+          Divider(),
+          _crearPassword(),
+           Divider(),
           _crearPersona(),
         ],
       ),
@@ -32,7 +37,6 @@ class _InputPageState extends State<InputPage> {
 
     return TextField(
       //autofocus: true, este comanando saca el teclado en automatico
-
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(
         border: OutlineInputBorder(
@@ -56,16 +60,68 @@ class _InputPageState extends State<InputPage> {
            _nombre = valor;
         });
       } ,
+    );
+  }
 
+  Widget _crearEmail(){
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          //personalizar que tan pronunciados quiero los border
+          borderRadius: BorderRadius.circular(20)
+        ),
+    
+        hintText: 'Email',
+        labelText: 'Email',
+        suffixIcon: Icon(Icons.alternate_email),
+        icon: Icon(Icons.email)
+
+
+      ),
+
+      onChanged: (valor)=>setState(() {
+           _email = valor;
+        })
+       ,
     );
 
   }
 
+  Widget _crearPassword(){
+    
+    return TextField(
+      obscureText: true,
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          //personalizar que tan pronunciados quiero los border
+          borderRadius: BorderRadius.circular(20)
+        ),
+    
+        hintText: 'Paswword',
+        labelText: 'Password',
+        suffixIcon: Icon(Icons.lock_open),
+        icon: Icon(Icons.lock)
+
+
+      ),
+
+      onChanged: (valor)=>setState(() {
+           _email = valor;
+        })
+       ,
+    );
+
+
+  }
+// imprime el nombre en pantalla en modo Binding
   Widget _crearPersona(){
     
     return ListTile(
       
       title: Text('Nombre es: $_nombre'),
+      subtitle: Text('Email: $_email'),
     );
   }
 }
