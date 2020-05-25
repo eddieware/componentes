7a440057ed7e38c5f92d02e162761693b2bfccce
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2020-05-25 08:00:05 -0500

    infiniteScroll

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -7,7 +7,31 @@ class ListaPage extends StatefulWidget {
 
 class _ListaPageState extends State<ListaPage> {
 
-  List<int> _listaNumeros =[1,2,3,4,5];
+//scroll controler es para poder detectar el fin y cargar mas
+
+ScrollController _scrollController = new ScrollController();
+
+  List<int> _listaNumeros = new List();
+  int _ultimoItem=0;
+
+  //para iniciar la clase initState y agregar el metodo cargar 10
+  @override
+  void initState() {
+    
+    super.initState();
+    _agregar10();
+
+    _scrollController.addListener(() {
+      //se dispara cada que se mueve el scroll
+      
+      if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent){
+        _agregar10();
+        
+      }
+
+     });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,12 +44,16 @@ class _ListaPageState extends State<ListaPage> {
 
   Widget _crearLista(){
     return ListView.builder(
+      controller: _scrollController, // se asigna para poder mover de posicion
       itemCount: _listaNumeros.length ,
       itemBuilder: (BuildContext context, int index){
+        //el index en el xonstructor se agrega porque se requiere
+        //la lista numeros se asocia con el index para alojarlos en imagen y pasarlos
+        //y pasarlos alinterpolationString
 
         final imagen = _listaNumeros[index];
-        print(_listaNumeros[index]);
-        print('Here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+       // print(_listaNumeros[index]);
+        //print('Here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
 
         return FadeInImage(
           
@@ -36,5 +64,18 @@ class _ListaPageState extends State<ListaPage> {
       },
       
       );
+  }
+
+  //mewtodo para agregar 10 imagenes mas a la lista
+
+  void _agregar10(){
+
+    for (var i = 0; i < 10; i++) {
+
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+      
+    }
+
   }
 }
